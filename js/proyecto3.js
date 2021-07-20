20ea1c37036517efff70d13faa3c1c173c9bbda9
Ailen Ariosto <ailen-1505@hotmail.com>
2021-07-20 09:49:33 -0300

    version 2.0

--- js/proyecto3.js
+++ js/proyecto3.js
@@ -1,84 +1,63 @@
-var tierra = {
-    nombre : 'Tierra',
-    peso : 9.8
-}
-
-var marte = {
-    nombre : 'Marte',
-    peso : 3.7,
-    imagen : 'https://i.imgur.com/s5N5B6N.png'
-}
-
-var jupiter = {
-    nombre : 'Jupiter',
-    peso : 24.8,
-    imagen : 'https://i.imgur.com/6uQx8Oy.png'
-}
-
-var saturno = {
-    nombre : 'Saturno',
-    peso : 10.44,
-    imagen : 'https://i.imgur.com/h9DAP9c.png'
-}
-
-var neptuno = {
-    nombre : 'Neptuno',
-    peso : 11.15,
-    imagen: 'https://i.imgur.com/uQrPE5a.png'
-}
-
-var urano = {
-    nombre : 'Urano',
-    peso : 8.87,
-    imagen : 'https://i.imgur.com/dxgpppG.png'
-}
-
-var venus = {
-    nombre : 'Venus',
-    peso : 8.87,
-    imagen : 'https://i.imgur.com/fls1ZTd.png'
-}
-
-var mercurio = {
-    nombre : 'Mercurio',
-    peso : 3.7,
-    imagen : 'https://i.imgur.com/yd5imjf.png'
-}
-
-var planetas = [tierra,mercurio,venus]
+let planetas = [
+    { nombre: 'Tierra', peso: 9.8, imagen: 'https://i.ibb.co/SRyvzvg/580b585b2edbce24c47b2705.png' },
+    { nombre: 'Marte', peso: 3.7, imagen: 'https://i.ibb.co/HtqzTXC/marte.png' },
+    { nombre: 'Jupiter', peso: 24.8, imagen: 'https://i.ibb.co/nc18srn/jupiter.png'},
+    { nombre: 'Saturno', peso: 10.44, imagen: 'https://i.ibb.co/jb76F7L/saturno.png'},
+    { nombre: 'Neptuno', peso: 11.15, imagen: 'https://i.ibb.co/n0TkjVF/neptuno.png'},
+    { nombre: 'Urano', peso: 8.87, imagen: 'https://i.ibb.co/HKnfMk0/urano.png'},
+    { nombre: 'Venus', peso: 8.87, imagen: 'https://i.ibb.co/bJ5kbG6/venus.png'},
+    { nombre: 'Mercurio', peso: 3.7, imagen: 'https://i.ibb.co/TqvXBTJ/mercurio.png'}
+]
 
 const pesoUsuario = () => {
     usuarioPeso = document.getElementById("peso").value;
-    document.getElementById('tierra').innerHTML='Tu Peso En La Tierra es de ' + usuarioPeso + ' Kilos';
 }
 
 const calcular = (planeta) => {
-    let pesoFinal = parseInt( this.usuarioPeso * ( planeta / tierra.peso ))
+    let pesoFinal = parseInt( this.usuarioPeso * ( planeta / 9.8 ))
     return pesoFinal
 }
 
 const calcularPesoPlanetas = () => {
+    document.getElementById('result').innerHTML = ""
     pesoUsuario();
-    let planetasOne = document.getElementById("listOne");
-    let planetasTwo = document.getElementById("listTwo");
-    
-    planetasOne.innerHTML = `
-        <ul class="main_sectionTwo_list">
-            <li><img class="main_sectionTwo__img" src="${ jupiter.imagen }" alt="jupiter" /> Tu peso en Jupiter sería: ${calcular(jupiter.peso)}</li>
-            <li><img  class="main_sectionTwo__img" src="${ mercurio.imagen }" alt="Mercurio" /> Tu peso en Mercurio sería: ${calcular(mercurio.peso)}</li>
-            <li><img  class="main_sectionTwo__img" src="${ neptuno.imagen }" alt="Neptuno" /> Tu peso en Neptuno sería:  ${calcular(neptuno.peso)}</li>
-            <li><img  class="main_sectionTwo__img" src="${ marte.imagen }" alt="Marte" /> Tu peso en Marte sería: ${calcular(marte.peso)}</li>
-        </ul>
-    `;
-    planetasTwo.innerHTML = `
-        <ul class="main_sectionTwo_list">
-            <li><img  class="main_sectionTwo__img" src="${ saturno.imagen }" alt="Saturno" /> Tu peso en Saturno sería: ${calcular(saturno.peso)} </li>
-            <li><img  class="main_sectionTwo__img" src="${ urano.imagen }" alt="Urano" /> Tu peso en Urano sería:  ${calcular(urano.peso)}</li>
-            <li><img class="main_sectionTwo__img" src="${ venus.imagen }" alt="Venus" /> Tu peso en Venus sería: ${calcular(venus.peso)}</li>
-        </ul>
-    `;
+    for(let p in planetas){
+        document.getElementById('result').innerHTML += `
+        <div>
+        <h4> ${ planetas[p].nombre} </h4>
+        <img class="main_sectionResult__img" src="${ planetas[p].imagen }" alt="${planetas[p].nombre}" /> 
+        <br>
+        <span>${calcular(planetas[p].peso)}
+        </div>
+        `
+    }
+
     let usuarioPeso = document.getElementById("peso").value="";
 }
 
+// Animacion del Botón 
 
-calcularPesoPlanetas();
+const button = document.getElementById('buttonContainer')
+button.onmouseover = function(){
+    showText()
+}
+
+const showText = ()=>{
+    document.getElementById('icon').classList.add('hideIcon')
+    document.getElementById('textButton').classList.add('showText')
+}
+
+button.onmouseleave = function() {
+    hideText()
+}
+
+const hideText = ()=>{
+    document.getElementById('textButton').classList.remove('showText')
+    document.getElementById('icon').classList.remove('hideIcon')
+}
+
+// Función del botón
+
+document.getElementById('calcular').onclick = function(){
+    calcularPesoPlanetas();
+}
